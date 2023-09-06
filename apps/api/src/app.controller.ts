a14ba882180ded3a44a3b9075e5a6078644106c5
ninthsun91 <ninthsun91@gmail.com>
2023-09-06 19:27:33 +0900

    :sparkles: Add endpoint GET /signin

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { TypedRoute } from '@nestia/core';
+import { v4 } from 'uuid';
 import { AppService } from './app.service';
+import { TryCatch, ERROR } from './types';
+import { GuestSigninResBody } from './dto';
 
 @Controller()
 export class AppController {
@@ -9,4 +13,22 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @TypedRoute.Get('/signin')
+  async guestSignIn(
+    @Res({ passthrough: true }) res: PublicResponse,
+  ): Promise<TryCatch<
+      GuestSigninResBody,
+      | typeof ERROR.UNAUTHORIZED
+  >> {
+    const userId = v4();
+
+    return createHttpResponse<GuestSigninResBody>({ userId });
+  }
 }
+
+export const createHttpResponse = <T>(data: T): ResponseForm<T> => ({
+  result: true,
+  code: 1000,
+  data,
+} as const);
